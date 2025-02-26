5206aab6004021ff01d7d9e16768522e3be5a993
KingKrouch <64479044+KingKrouch@users.noreply.github.com>
2025-02-26 00:15:57 -0500

    Update this to use NAME rather than DISTRO_ID
    Reason being that we get "NixOS" or "Fedora Linux" rather than lowercase crap.

--- Build_Release.sh
+++ Build_Release.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Check the distribution ID in /etc/*release
-DISTRO_ID=$(cat /etc/*release | grep ^DISTRIB_ID= | cut -d '=' -f 2 | tr -d '"')
+DISTRO_NAME=$(cat /etc/*release | grep ^NAME= | cut -d '=' -f 2 | tr -d '"')
 
-# Check if the system is NixOS by looking for "nixos"
-if [ "$DISTRO_ID" == "nixos" ]; then
+# Check if the system is NixOS by looking for "NixOS"
+if [ "$DISTRO_NAME" == "NixOS" ]; then
   echo "This system is NixOS. Building through nix-build..."
   NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1 nix-build --arg buildType '"Release"'
 else
